45906274cdb632e93e795e0de4b82cbd3d4d1673
justxjames <100766138+justxjames@users.noreply.github.com>
2024-07-29 20:53:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
